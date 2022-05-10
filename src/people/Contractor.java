b4b8d041cbb60f700707dcffec81cfb504bb6001
Jakub Gramburg <jacubgr@gmail.com>
2022-05-10 21:21:04 +0200

    add contractor class

---
+++ src/people/Contractor.java
@@ -0,0 +1,12 @@
+package people;
+
+import people.enums.Position;
+import people.enums.Seniority;
+
+public class Contractor extends Human{
+
+    private final Position position;
+    private final Seniority seniority;
+
+
+}
