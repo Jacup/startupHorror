0bc889dc4a569472c0f47cb66484bab0c427f302
Jakub Gramburg <jacubgr@gmail.com>
2022-05-10 21:46:57 +0200

    some next init classes

---
+++ src/main/Company.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Company {
+    private final String domain;
+
+    public Company(String domain) {
+        this.domain = domain;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+}
