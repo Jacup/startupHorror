8ac5a608ba5f1324831f3f42e4b532fa3030995e
Jakub Gramburg <jacubgr@gmail.com>
2022-05-11 21:01:56 +0200

    add junit libs

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,45 @@
 package main;
 
+import jobs.Project;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
+    private static final int START_PROJECTS = 3;
+
+    private static final ArrayList<Project> availableProjects = new ArrayList();
+
     public static void main(String[] args) {
-	// write your code here
+        initializeData();
+
+        startGame();
+    }
+
+    private static void initializeData() {
+        generateEmployees();
+        for (int i = 0; i < START_PROJECTS; i++) {
+            availableProjects.add(Project.generateRandomProject());
+        }
+    }
+
+    private static void startGame() {
+        // creates user company
+        System.out.println("Enter your startup name:\n");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+
+        createCompany(name);
+
+    }
+
+    private static void createCompany(String name) {
+        Company company = new Company(name);
+    }
+
+    private static void generateEmployees() {
+        // generates random 3 employees to hire at start
     }
 }
