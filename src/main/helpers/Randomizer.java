d3f9687f2ffa4029f7189ce94c6fb0243cf93b71
Jakub Gramburg <jacubgr@gmail.com>
2022-05-12 19:12:00 +0200

    new method to generate random variables

--- src/main/helpers/Randomizer.java
+++ src/main/helpers/Randomizer.java
@@ -1,11 +1,21 @@
 package main.helpers;
 
+import main.jobs.enums.TechStack;
+
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Randomizer {
 
     public static int generateRandomValue(int min, int max) {
         return ThreadLocalRandom.current().nextInt(min, max + 1);
-
     }
+
+    public static int generateRandomValue(int max) {
+        return ThreadLocalRandom.current().nextInt(max);
+    }
+
+    public static boolean draw(int percentage) {
+        return ThreadLocalRandom.current().nextInt(100) <= percentage;
+    }
+
 }
